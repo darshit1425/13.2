e42be63d7513e42ffac502836f41a40983dd2458
r51 <darshitmendapara1425@gmail.com>
2023-01-19 23:12:15 -0800

    13.2

--- lib/Page1.dart
+++ lib/Page1.dart
@@ -12,12 +12,16 @@ class _HomePageState extends State<HomePage> {
   bool lan2 = false;
   bool lan3 = false;
   bool lan4 = false;
+  String selected = "Android";
+  RangeValues rangeValues = RangeValues(10, 1000);
+  bool on1 = false;
+  bool on2 = false;
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        backgroundColor: Colors.white12,
+        backgroundColor: Colors.grey.shade200,
         appBar: AppBar(
           title: Text(
             " App ",
@@ -31,85 +35,237 @@ class _HomePageState extends State<HomePage> {
           ),
           backgroundColor: Colors.black,
         ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              "Languages",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Row(
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Checkbox(
-                    value: lan1,
-                    onChanged: (value) {
-                      setState(() {
-                        lan1 = value!;
-                      });
-                    }),
+                SizedBox(
+                  height: 10,
+                ),
                 Text(
-                  "English",
-                  style: TextStyle(fontSize: 16.5),
+                  "Languages",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: [
+                    Checkbox(
+                      value: lan1,
+                      onChanged: (value) {
+                        setState(
+                          () {
+                            lan1 = value!;
+                          },
+                        );
+                      },
+                    ),
+                    Text(
+                      "English",
+                      style: TextStyle(fontSize: 16.5),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: [
+                    Checkbox(
+                      value: lan2,
+                      onChanged: (value) {
+                        setState(
+                          () {
+                            lan2 = value!;
+                          },
+                        );
+                      },
+                    ),
+                    Text(
+                      "Hindi",
+                      style: TextStyle(fontSize: 16.5),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: [
+                    Checkbox(
+                      value: lan3,
+                      onChanged: (value) {
+                        setState(
+                          () {
+                            lan3 = value!;
+                          },
+                        );
+                      },
+                    ),
+                    Text(
+                      "Gujarati",
+                      style: TextStyle(fontSize: 16.5),
+                    ),
+                  ],
+                ),
+                CheckboxListTile(
+                  value: lan4,
+                  onChanged: (value) {
+                    setState(
+                      () {
+                        lan4 = value!;
+                      },
+                    );
+                  },
+                  title: Text("Arbi"),
+                ),
+                ListTile(
+                  leading: Text("1"),
+                  title: Text("ELON"),
+                  subtitle: Text("Tesla"),
+                  trailing: Text("CA"),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "Technology",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                Row(
+                  children: [
+                    Radio(
+                      value: "Android",
+                      groupValue: selected,
+                      onChanged: (value) {
+                        setState(
+                          () {
+                            selected = value!;
+                          },
+                        );
+                      },
+                    ),
+                    Text("Android"),
+                  ],
+                ),
+                SizedBox(
+                  height: 2,
+                ),
+                Row(
+                  children: [
+                    Radio(
+                      value: "IOS",
+                      groupValue: selected,
+                      onChanged: (value) {
+                        setState(
+                          () {
+                            selected = value!;
+                          },
+                        );
+                      },
+                    ),
+                    Text("IOS"),
+                  ],
+                ),
+                SizedBox(
+                  height: 2,
+                ),
+                Row(
+                  children: [
+                    Radio(
+                      value: "Web",
+                      groupValue: selected,
+                      onChanged: (value) {
+                        setState(
+                          () {
+                            selected = value!;
+                          },
+                        );
+                      },
+                    ),
+                    Text("Web"),
+                  ],
+                ),
+                SizedBox(
+                  height: 2,
+                ),
+                RadioListTile(
+                  value: "Flutter",
+                  groupValue: selected,
+                  onChanged: (value) {
+                    setState(
+                      () {
+                        selected = value!;
+                      },
+                    );
+                  },
+                  title: Text("Flutter"),
+                ),
+                SizedBox(
+                  height: 2,
+                ),
+                Text(
+                  "slider",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 2,
+                ),
+                RangeSlider(
+                  values: rangeValues,
+                  max: 10000,
+                  divisions: 100,
+                  labels: RangeLabels(
+                    rangeValues.start.round().toString(),
+                    rangeValues.end.round().toString(),
+                  ),
+                  onChanged: (value) {
+                    setState(
+                      () {
+                        rangeValues = value!;
+                      },
+                    );
+                  },
+                ),
+                SizedBox(
+                  height: 2,
+                ),
+                Text(
+                  "Switch",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Switch(
+                  value: on1,
+                  onChanged: (value) {
+                    setState(
+                      () {
+                        on1 = value!;
+                      },
+                    );
+                  },
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Switch(
+                  value: on2,
+                  onChanged: (value) {
+                    setState(
+                      () {
+                        on2 = value!;
+                      },
+                    );
+                  },
                 ),
               ],
             ),
-            SizedBox(
-              height: 10,
-            ),
-            Row(
-              children: [
-                Checkbox(
-                    value: lan2,
-                    onChanged: (value) {
-                      setState(() {
-                        lan2 = value!;
-                      });
-                    }),
-                Text(
-                  "Hindi",
-                  style: TextStyle(fontSize: 16.5),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Row(
-              children: [
-                Checkbox(
-                    value: lan3,
-                    onChanged: (value) {
-                      setState(() {
-                        lan3 = value!;
-                      });
-                    }),
-                Text(
-                  "Gujarati",
-                  style: TextStyle(fontSize: 16.5),
-                ),
-              ],
-            ),
-            CheckboxListTile(
-              value: lan4,
-              onChanged: (value) {
-                setState(() {
-                  lan4 = value!;
-                });title: Text("Arbi");
-              },
-            ),
-            // ListTile(
-            //   leading: Text("1"),
-            //   title: Text("ELON"),
-            //   subtitle: Text("Tesla"),
-            //   trailing: Text("CA"),
-            // ),
-          ],
+          ),
         ),
       ),
     );
